59a1ad99560635c0647f0b3bb801af3b7c9bf79a
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2024-10-28 19:34:53 +0600

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,5 @@
+import random;
+option=["Good","Better","Best","Bad"];
+question=input("Is only eating vegitable being a healthy diet?")
+answer=random.choice(option)
+print(f"The answer is:{answer}")
